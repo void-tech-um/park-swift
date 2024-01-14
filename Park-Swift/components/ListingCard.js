c298bd5ffa721d53b848b6bedf262fad7778a2f6
Mann Gupta <manng@umich.edu>
2024-01-14 17:20:17 -0500

    added ListingCard component

---
+++ Park-Swift/components/ListingCard.js
@@ -0,0 +1,61 @@
+import * as React from "react";
+
+import { ImageBackground, StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+
+
+//listing card component that displays the listing
+const ListingCard = () => {
+    //take in data from backend
+    
+    return (
+        <View style={styles.container}>
+            {/* //update and display specific info variables
+            //1. price per hour
+            //2. address
+            //3. date
+            //4. start time
+            //5. end time
+            //contact button */}
+            <TouchableOpacity>
+                <Text style={styles.button}>Hi</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        flexDirection: "row",
+        height: "33%",
+        width: "100%",
+        backgroundColor: "white",
+        alignItems: "center",
+    },
+    image: {
+        height: "33%",
+        width: "100%",
+    },
+    infoContainer: {
+        height: "33%",
+        width: "100%",
+        backgroundColor: "gray",
+    },
+    bannerContainer: {
+        height: "33%",
+        width: "100%",
+    },
+    address: {
+
+    },
+    button: {
+        alignItems: "center",
+        padding: 10,
+        justifyContent: "center",
+    },
+    banner: {
+
+    },
+});
+
+export default ListingCard;
