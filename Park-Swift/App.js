22392c17995d78dfce435cf41aeb7b6838bf2754
Jennifer Weir <jennweir@umich.edu>
2024-01-20 01:41:42 -0500

    movement to login and registration screens working

--- Park-Swift/App.js
+++ Park-Swift/App.js
@@ -1,72 +1,229 @@
+import 'react-native-gesture-handler';
+// import React, { useEffect, useState } from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+// import { LoginScreen, HomeScreen, RegistrationScreen } from './screens'
+// import {decode, encode} from 'base-64'
+// if (!global.btoa) {  global.btoa = encode }
+// if (!global.atob) { global.atob = decode }
+
 import { StyleSheet, Text, View } from 'react-native';
 
 // In App.js in a new project
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import CurrentTile from './screens/home';
 import SearchBar from './screens/search';
+import { Image, TextInput, TouchableOpacity } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
+const Stack = createStackNavigator();
 
-const Tab = createMaterialTopTabNavigator();
+function LoginScreen({navigation}) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-function HomeScreen() {
-  // adding currentTile and search bar components
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <React.Fragment>
-        <SearchBar/>
-        <CurrentTile/>
-      </React.Fragment>
-    </View>
-  );
+    const onFooterLinkPress = () => {
+        navigation.navigate('Registration')
+    }
+
+    const onLoginPress = () => {
+    }
+
+    return (
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+                <TextInput
+                    style={styles.input}
+                    placeholder='E-mail'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Password'
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => onLoginPress()}>
+                    <Text style={styles.buttonTitle}>Log in</Text>
+                </TouchableOpacity>
+                <View style={styles.footerView}>
+                    <Text style={styles.footerText}>Don't have an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Sign up</Text></Text>
+                </View>
+            </KeyboardAwareScrollView>
+        </View>
+    )
 }
 
-function MapScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Map Screen</Text>
-    </View>
-  );
+function RegistrationScreen({navigation}) {
+    const [fullName, setFullName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const onFooterLinkPress = () => {
+        navigation.navigate('Login')
+    }
+
+    const onRegisterPress = () => {
+    }
+
+    return (
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+                <TextInput
+                    style={styles.input}
+                    placeholder='Full Name'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setFullName(text)}
+                    value={fullName}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='E-mail'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Password'
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Confirm Password'
+                    onChangeText={(text) => setConfirmPassword(text)}
+                    value={confirmPassword}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => onRegisterPress()}>
+                    <Text style={styles.buttonTitle}>Create account</Text>
+                </TouchableOpacity>
+                <View style={styles.footerView}>
+                    <Text style={styles.footerText}>Already got an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Log in</Text></Text>
+                </View>
+            </KeyboardAwareScrollView>
+        </View>
+    )
 }
 
+export default function App() {
 
-function MessagesScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Messages Screen</Text>
-    </View>
-  );
-}
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
 
-function ProfileScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile Screen</Text>
-    </View>
-  );
-}
-
-function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="HomeScreen"
-      tabBarPosition="bottom"
-      screenOptions={{
-        tabBarLabelStyle: { fontSize: 12 },
-        tabBarItemStyle: { width: 100 },
-        tabBarStyle: { backgroundColor: 'powderblue' },
-      }}>
-      <Tab.Screen name="Map" component={MapScreen} />
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Messages" component={MessagesScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+      <Stack.Navigator>
+        { user ? (
+          <Stack.Screen name="Home">
+            {props => <HomeScreen {...props} extraData={user} />}
+          </Stack.Screen>
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Registration" component={RegistrationScreen} />
+          </>
+        )}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-export default App;
+
+
+// const Tab = createMaterialTopTabNavigator();
+
+
+// function HomeScreen() {
+//   // adding currentTile and search bar components
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <React.Fragment>
+//         <SearchBar/>
+//         <CurrentTile/>
+//       </React.Fragment>
+//     </View>
+//   );
+// }
+
+// function MapScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Map Screen</Text>
+//     </View>
+//   );
+// }
+
+
+// function MessagesScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Messages Screen</Text>
+//     </View>
+//   );
+// }
+
+// function ProfileScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Profile Screen</Text>
+//     </View>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator initialRouteName="HomeScreen"
+//       tabBarPosition="bottom"
+//       screenOptions={{
+//         tabBarLabelStyle: { fontSize: 12 },
+//         tabBarItemStyle: { width: 100 },
+//         tabBarStyle: { backgroundColor: 'powderblue' },
+//       }}>
+//         <Tab.Screen name="Map" component={MapScreen} />
+//         <Tab.Screen name="LoginScreen" component={LoginScreen} />
+//         <Tab.Screen name="RegistrationScreen" component={RegistrationScreen} />
+//         <Tab.Screen name="Home" component={HomeScreen} />
+//         <Tab.Screen name="Messages" component={MessagesScreen} />
+//         <Tab.Screen name="Profile" component={ProfileScreen} />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// export default App;
 
 const styles = StyleSheet.create({
   //height: Platform.OS === 'ios' ? 200 : 100, // example of platform specific styling!
@@ -81,13 +238,13 @@ const styles = StyleSheet.create({
 
 
 
-//Search Bar
+// //Search Bar
 
 
 
 
 
-// creating box component
+// // creating box component
 
 
 
