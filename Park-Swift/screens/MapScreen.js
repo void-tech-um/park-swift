4872d9773d3bd54f89ec4d309eab364204a87756
Aarushi <aarushsi@umich.edu>
2024-03-13 20:35:24 -0400

    stuff

--- Park-Swift/screens/MapScreen.js
+++ Park-Swift/screens/MapScreen.js
@@ -1,13 +1,34 @@
-import * as React from 'react';
-import {Text,View} from 'react-native';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 
-function MapScreen() {
+  const Map = () => {
+    const position = [51.505, -0.09]; // Example: London's coordinates
+
+    // Fix for default marker icons not showing due to path issues in React
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    });
+
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Map Screen</Text>
-      </View>
+      <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     );
-}
+  };
+
 
 export default MapScreen;
