6eba22235a01c008e64ab9d4edab4b5327f50756
Brian <zhangbri@umich.edu>
2024-05-22 17:58:00 -0400

    Made a listingsData.js file in components folder in order to reuse the listingsData and listingCard for homeScreen.js and SavedListings.js

--- Park-Swift/screens/HomeScreen.js
+++ Park-Swift/screens/HomeScreen.js
@@ -1,90 +1,45 @@
 import React from 'react';
-import { View, StyleSheet, Text, TouchableWithoutFeedback, Keyboard, ScrollView, FlatList, SafeAreaView } from 'react-native';
+import { View, StyleSheet, Text, TouchableWithoutFeedback, Keyboard, ScrollView, Dimensions } from 'react-native';
 import SortingButton from '../components/SortingButton2';
 import ListingCard from '../components/ListingCard';
 import CurrentlyRentingCard from '../components/CurrentlyRenting';
-import SearchBar from '../screens/search.js';
 import MenuSearchBar from './search';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Dimensions } from 'react-native';
+import SavedListings from './SavedListings';
+import listingsData from '../components/listingsData';
 
 const windowHeight = Dimensions.get('window').height;
 
-const listingsData = [
-  {
-      id: 1,
-      address: "419 S State St, Ann Arbor",
-      ppHour: "$10.50 /hr",
-      date: "11/19/2023",
-      startTime: "12:00PM",
-      endTime: "3:00PM",
-      image: ('/images/landscape.jpg'),
-      listingURL: "/",
-  },
-  {
-    id: 2,
-    address: "419 S State St, Ann Arbor",
-    ppHour: "$10.50 /hr",
-    date: "11/19/2023",
-    startTime: "12:00PM",
-    endTime: "3:00PM",
-    image: ('/images/landscape.jpg'),
-    listingURL: "/",
-  },
-  {
-    id: 3,
-    address: "419 S State St, Ann Arbor",
-    ppHour: "$10.50 /hr",
-    date: "11/19/2023",
-    startTime: "12:00PM",
-    endTime: "3:00PM",
-    image: ('/images/landscape.jpg'),
-    listingURL: "/",
-  },
-  {
-    id: 4,
-    address: "419 S State St, Ann Arbor",
-    ppHour: "$10.50 /hr",
-    date: "11/19/2023",
-    startTime: "12:00PM",
-    endTime: "3:00PM",
-    image: ('/images/landscape.jpg'),
-    listingURL: "/",
-  },
-]
-
 const HomeScreen = () => {
   const insets = useSafeAreaInsets();
-  const listingCardHeight = windowHeight * 0.2; // replace 0.2 with the actual percentage
+  const listingCardHeight = windowHeight * 0.2;
 
   return (
-      <View style={{ flex: 1 }}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss} 
-                          accessible={false}>
-          <MenuSearchBar />
-        </TouchableWithoutFeedback>
-        <CurrentlyRentingCard />
-        <View style={styles.headerContainer}>
+    <View style={{ flex: 1 }}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <MenuSearchBar />
+      </TouchableWithoutFeedback>
+      <CurrentlyRentingCard />
+      <View style={styles.headerContainer}>
         <Text style={styles.listingsNearYouText}>Listings For You</Text>
         <SortingButton />
       </View>
-        <ScrollView contentContainerStyle={{ flexGrow: 1, paddingBottom: insets.bottom + listingCardHeight }} 
-          showsVerticalScrollIndicator={false}
-          showsHorizontalScrollIndicator={false}>
-          {listingsData.map((listing) => (
-            <ListingCard
-              key={listing.id}
-              address={listing.address}
-              date={listing.date}
-              startTime={listing.startTime}
-              endTime={listing.endTime}
-              image={listing.image}
-              ppHour={listing.ppHour}
-              listingURL={listing.listingURL}
-            />
-          ))}
-        </ScrollView>
-      </View>
+      <ScrollView contentContainerStyle={{ flexGrow: 1, paddingBottom: insets.bottom + listingCardHeight }} showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}>
+        {listingsData.map((listing) => (
+          <ListingCard
+            key={listing.id}
+            address={listing.address}
+            date={listing.date}
+            startTime={listing.startTime}
+            endTime={listing.endTime}
+            image={listing.image}
+            ppHour={listing.ppHour}
+            listingURL={listing.listingURL}
+          />
+        ))}
+      </ScrollView>
+      <SavedListings listingsData={listingsData} />
+    </View>
   );
 };
 
@@ -92,13 +47,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
-  },
-  searchBarContainer: {
-    position:"absolute",
-    // marginTop: 60,
-    width: '100%',
-    justifyContent: 'center',
-    alignSelf: 'center',
   },
   headerContainer: {
     flexDirection: 'row',
@@ -113,7 +61,6 @@ const styles = StyleSheet.create({
     fontSize: 25,
     color: 'black',
   },
-  // No additional or complex styling to affect layout
 });
 
 export default HomeScreen;
