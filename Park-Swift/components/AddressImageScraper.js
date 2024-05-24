01468fa80c45efc105402fe4658739a568892245
Mann Gupta <manng@umich.edu>
2024-05-24 13:50:16 -0400

    AddressImageScraper cleanup

--- Park-Swift/components/AddressImageScraper.js
+++ Park-Swift/components/AddressImageScraper.js
@@ -4,7 +4,9 @@ import { View, Image, Text, StyleSheet} from 'react-native';
 import axios from 'axios';
 import { Card } from 'react-native-elements';
 
-const AddressMapping = () => { 
+//THIS IS UNFINISHED, IT SEEMS TO BE FOR GETTING THE IMAGE FOR LISTINGS AUTOMATICALLY BY THE ADDRESS
+
+const AddressImageScraper = () => { 
   return (
     <View style={styles.container}>
       <Text style={styles.caption}>Address 123</Text>
@@ -73,6 +75,6 @@ const styles = StyleSheet.create({
 //   );
 // };
 
-export default AddressMapping;
+export default AddressImageScraper;
 
 
