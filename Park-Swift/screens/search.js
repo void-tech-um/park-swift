5af9e285b27bae7eca9baa1672c0c5d2e3a624db
Brian <zhangbri@umich.edu>
2024-05-26 18:56:44 -0400

    Updated search screen

--- Park-Swift/screens/search.js
+++ Park-Swift/screens/search.js
@@ -1,72 +1,59 @@
 import React, { useState } from 'react';
-import { View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
-import { AntDesign, Ionicons } from '@expo/vector-icons'; 
-import HamburgerMenu from './hamburgerMenu.js';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import {Searchbar} from 'react-native-paper';
-import { TouchableWithoutFeedback, Keyboard } from 'react-native';
-
+import { Searchbar } from 'react-native-paper';
+import HamburgerMenu from './hamburgerMenu.js';
 
 const MenuSearchBar = () => {
-    const [searchText, setSearchText] = useState('');
-    const handleSearch = (text) => {
-        setSearchText(text);
-    };
-
     const [isMenuVisible, setMenuVisible] = useState(false);
 
     const toggleMenu = () => {
-      setMenuVisible(!isMenuVisible);
+        setMenuVisible(!isMenuVisible);
     };
-    
-    const [searchQuery, setSearchQuery] = React.useState('');
+
+    const [searchQuery, setSearchQuery] = useState('');
+
     return (
-        <View style = {styles_searchbar.container}>
-          <TouchableOpacity style={styles_searchbar.iconContainer}>
-            <MaterialCommunityIcons name="menu" color={'white'} size={36} onPress={toggleMenu}/>
-          </TouchableOpacity>
-          <HamburgerMenu isVisible={isMenuVisible} onClose={toggleMenu} />
-          <Searchbar
-              lightTheme
-              placeholder="Search"
-              onChangeText={setSearchQuery}
-              inputStyle={{top:'-2.5%'}}
-              value={searchQuery}
-              style={styles_searchbar.input}
-          />
-          {/* <TextInput 
-            style = {styles_searchbar.input}
-            placeholder = "Search address"
-            placeholderTextColor = "black"
-            onChangeText = {handleSearch}
-            value = {searchText}
-          /> */}
+        <View style={styles_searchbar.container}>
+            <TouchableOpacity style={styles_searchbar.iconContainer}>
+                <MaterialCommunityIcons name="menu" color={'white'} size={48} onPress={toggleMenu} />
+            </TouchableOpacity>
+            <HamburgerMenu isVisible={isMenuVisible} onClose={toggleMenu} />
+            <Searchbar
+                placeholder="Search Address"
+                onChangeText={setSearchQuery}
+                value={searchQuery}
+                style={styles_searchbar.input}
+                inputStyle={styles_searchbar.inputText}
+                placeholderTextColor="#A3A3A3"
+            />
         </View>
     );
-  };
+};
 
-    const styles_searchbar = StyleSheet.create({
-        container: {
-          position:'flex',
-          justifyContents:'space-around',
-          flexDirection: 'row',
-          alignItems: 'left',
-          backgroundColor: '#033566', // Dark grey background
-          padding: '3%',
-          paddingTop:'12.5%',
-        },
-        input: {
-          flex: 8,
-          backgroundColor: '#f5f5f5',
-          color:'#a3a3a3', 
-          alignSelf: 'center',
-          height:'98%',
-        },
-        iconContainer: {
-          flex:1,
-          padding: 5,
-          
-        },
-      });
+const styles_searchbar = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#052658',
+        padding: '3%',
+        paddingTop: '12.5%',
+    },
+    iconContainer: {
+        marginLeft: 10,
+    },
+    input: {
+        backgroundColor: '#f5f5f5',
+        alignSelf: 'center',
+        height: 41,
+        width: 303,
+        marginLeft: 'auto',
+        marginRight: 15,
+    },
+    inputText: {
+        fontSize: 20,
+        marginVertical: -10,
+    },
+});
 
 export default MenuSearchBar;
