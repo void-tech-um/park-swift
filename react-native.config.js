003bb278c92e93c00b1342f98ebf118daa2cd549
Brian Zhang <zhangbri@umich.edu>
2025-05-01 22:38:46 -0400

    ios credentials, xcode simulator not working

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dependencies: {
+      'react-native-maps': {
+        platforms: {
+          ios: null, // disables iOS auto-linking, including Fabric-related codegen
+        },
+      },
+    },
+  };
+  
